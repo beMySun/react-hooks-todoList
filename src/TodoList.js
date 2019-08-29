00cb958997b9005e949d617775ba1cabc0bf5314
KK <yongkang.yan@shopee.com>
2019-08-29 21:08:38 +0800

    Feat: add  update api

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,27 +1,61 @@
 import React, { useState, useEffect } from 'react';
-import { useTodos } from './Store.js'
+import axios from 'axios'
+const uuidv4 = require('uuid/v4');
+// import { useTodos } from './Store.js'
 
 export function TodoList() {
-  const [todos, updateTodos] = useTodos()
 
-  const deleteTodo = id => updateTodos(todos => {
-    const todoIdxToDelete = todos.findIndex(todo => todo.id === id)
-    todos.splice(todoIdxToDelete, 1)
-  })
+  const [loading, setLoading] = useState(true)
+  const [todoList, updateTodoList] = useState([])
 
-  const completeTodo = id => updateTodos(todos => {
-    todos.find(todo => todo.id === id).isCompleted = true
-  })
+  const completeTodo = (id, isCompleted) => {
+    setLoading(true)
+    axios.post("http://localhost:8080/updateRecord", { id, isCompleted: Number(!isCompleted) })
+      .then(() => { queryRecords() })
+  }
+
+  useEffect(() => {
+    queryRecords()
+  }, []);
+
+  const queryRecords = () => {
+    setLoading(true)
+    fetch("http://localhost:8080/getTodoList")
+      .then(response => response.json())
+      .then(result => updateTodoList(result.data))
+      .then(() => setLoading(false))
+  }
+
+  const addTodo = () => {
+    const newRecord = {
+      id: uuidv4(),
+      text: 'some text ' + uuidv4(),
+      isCompleted: 0,
+    }
+    setLoading(true)
+    axios.post("http://localhost:8080/CreateTodoRecord", newRecord)
+      .then(() => { queryRecords() })
+  }
+
+  const deleteTodo = id => {
+    setLoading(true)
+    axios.post("http://localhost:8080/deleteRecord", { id })
+      .then(() => { queryRecords() })
+  }
 
   return (
-    <ul>
-      {todos.map(todo => (
-        <li key={todo.id} style={todo.isCompleted ? { color: 'red' } : { color: 'green' }}>
-          <span>{todo.title}</span>
-          <button onClick={() => deleteTodo(todo.id)}>Delete</button>
-          <button onClick={() => completeTodo(todo.id)}>Complete</button>
-        </li>
-      ))}
-    </ul>
+    <>
+      {loading ? <div> Loading </div> : <div> loaded</div>}
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todoList.map(todo => (
+          <li key={todo.id} style={todo.isCompleted ? { color: 'red' } : { color: 'green' }}>
+            <span>{todo.text || '/'}</span>
+            <button onClick={() => completeTodo(todo.id, todo.isCompleted)}>Complete</button>
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
